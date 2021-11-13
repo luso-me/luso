11812ab8fa806c2a6e2c9e713f9e0f08b24a2ed4
Daniel Ng See Cheong <daniel@ngseecheong.co.za>
2021-11-13 15:47:05 +0200

    Simple test

---
+++ tests/api/test_skill.py
@@ -0,0 +1,9 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_root(async_client: AsyncClient):
+    response = await async_client.get("/skills/")
+    assert response.status_code == 200
+    assert response.json() == []
