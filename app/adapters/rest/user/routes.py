e5ea95076390b28fb406266083f979967e278715
zwessels <zak@wcisolutions.com>
2022-03-01 08:56:43 +0200

    #18 incorporate black to run automatically

--- app/adapters/rest/user/routes.py
+++ app/adapters/rest/user/routes.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
 
-from . import base, skill
+from . import base
 
 router = APIRouter(tags=['user'], redirect_slashes=False)
 
-router.include_router(base.router,)
-
-router.include_router(skill.router,)
+router.include_router(base.router, )
