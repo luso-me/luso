f137b3283ec1768d5f23b121c23e6c387db0439b
Daniel Ng See Cheong <daniel@ngseecheong.co.za>
2022-03-13 15:06:47 +0200

    #25 Use skill service from api

--- app/adapters/rest/skill/base.py
+++ app/adapters/rest/skill/base.py
@@ -5,13 +5,10 @@ import structlog
 from fastapi import HTTPException, status, APIRouter, Depends, UploadFile
 
 from app.adapters.dependencies.auth import get_current_user
-from app.adapters.dependencies.db import skill_repository
-from app.core.media import media_service
-from app.core.media.media_service import MediaService
-from app.core.skill import skill_service
+
 from app.core.skill.model.base import SkillCreate, SkillRead, SkillUpdate
+from app.core.skill.skill_service import SkillService
 from app.core.user.model.base import UserRead
-from app.repositories.skill import SkillRepository
 
 log = structlog.get_logger()
 
@@ -25,8 +22,8 @@ router = APIRouter(prefix="/skills")
     status_code=status.HTTP_201_CREATED,
 )
 async def create_skill(
-    skill: SkillCreate, current_user: UserRead = Depends(get_current_user)
-):
+    skill: SkillCreate, current_user: UserRead = Depends(get_current_user),
+        skill_service: SkillService = Depends(SkillService)):
     log.debug(f"attempting to create skill with body {skill}")
     return await skill_service.create_skill(skill)
 
@@ -34,10 +31,10 @@ async def create_skill(
 @router.get("", response_description="List all skills", response_model=List[SkillRead])
 async def list_skills(
     limit: int = 100,
-    skill_repo: SkillRepository = Depends(skill_repository),
+    skill_service: SkillService = Depends(SkillService),
     current_user: UserRead = Depends(get_current_user),
 ):
-    skills = await skill_repo.list(limit)
+    skills = await skill_service.list_skills(limit)
     log.debug("skills found", skills=skills)
     return skills
 
@@ -48,12 +45,12 @@ async def list_skills(
 async def find_query(
     skill_name: Optional[str] = None,
     limit: int = 10,
-    skill_repo: SkillRepository = Depends(skill_repository),
+    skill_service: SkillService = Depends(SkillService),
     current_user: UserRead = Depends(get_current_user),
 ):
     if skill_name:
         log.info("searching for skills with name", skill_name=skill_name)
-        return await skill_repo.find({"name": skill_name}, limit=limit)
+        return await skill_service.find_skill(skill_name, limit)
 
     raise HTTPException(
         status_code=HTTPStatus.BAD_REQUEST, detail="No known search parameter passed"
@@ -65,10 +62,10 @@ async def find_query(
 )
 async def show_skill(
     skill_id: str,
-    skill_repo: SkillRepository = Depends(skill_repository),
+    skill_service: SkillService = Depends(SkillService),
     current_user: UserRead = Depends(get_current_user),
 ):
-    if (skill := await skill_repo.get(skill_id)) is not None:
+    if (skill := await skill_service.get_skill(skill_id)) is not None:
         return skill
 
     raise HTTPException(status_code=404, detail=f"skill {skill_id} not found")
@@ -80,23 +77,22 @@ async def show_skill(
 async def update_skill(
     skill_id: str,
     skill: SkillUpdate,
+    skill_service: SkillService = Depends(SkillService),
     current_user: UserRead = Depends(get_current_user),
 ):
     return await skill_service.update_skill(skill_id, skill)
 
 
-@router.delete("/{skill_id}", response_description="Delete a skill")
+@router.delete("/{skill_id}", response_description="Delete a skill", status_code=HTTPStatus.NO_CONTENT)
 async def delete_skill(
     skill_id,
-    skill_repo: SkillRepository = Depends(skill_repository),
+    skill_service: SkillService = Depends(SkillService),
     current_user: UserRead = Depends(get_current_user),
 ):
-    await skill_repo.delete(skill_id)
+    await skill_service.delete_skill(skill_id)
 
 
 @router.post("/{skill_id}/icon")
-async def skill_icon_upload(skill_id: str, file: UploadFile, skill_repo: SkillRepository = Depends(skill_repository), media_service: MediaService = Depends(MediaService)):
+async def skill_icon_upload(skill_id: str, file: UploadFile, skill_service: SkillService = Depends(SkillService)):
     log.info("file upload", filename=file.filename)
-    skill = SkillUpdate()
-    skill.icon_link = await media_service.upload_image(file.filename, file.file)
-    return await skill_repo.update(skill_id, skill)
+    return await skill_service.update_skill_icon(skill_id, file.filename, file.file)
