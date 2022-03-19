eb57a430e95d95782dd88dcac03f361cda19ab5f
zwessels <zak@wcisolutions.com>
2022-03-19 23:00:19 +0200

    #27 fix media service bug

--- app/core/media/media_service.py
+++ app/core/media/media_service.py
@@ -10,19 +10,25 @@ log = structlog.get_logger()
 
 
 class MediaService:
-    def __init__(self, region: str, bucket_name: str):
+    def __init__(self, region: str, bucket_name: str, random_suffix=False):
         self.s3 = boto3.resource("s3", config=Config(region_name=region))
         self.region = region
         self.bucket_name = bucket_name
         self.bucket = self.s3.Bucket(bucket_name)
+        self.random_suffix = random_suffix
+        self.s3_url = f"https://{self.bucket_name}.s3.{self.region}.amazonaws.com"
 
     async def upload_image(self, image_name: str, bytes_: IO) -> str:
         log.info(f"Uploading image, {image_name} to S3")
 
+        suffix = ""
+        if self.random_suffix:
+            suffix = shortuuid.random()
+
         basename, ext = os.path.splitext(image_name)
         self.bucket.upload_fileobj(bytes_, f"{basename}.{ext}")
 
-        return (
-            f"https://{self.bucket_name}.s3.{self.region}.amazonaws.com"
-            f"/{basename}{ext}"
-        )
+        if suffix:
+            return f"{self.s3_url}/{basename}-{suffix}{ext}"
+        else:
+            return f"{self.s3_url}/{basename}{ext}"
--- tests/core/media/test_media_service.py
+++ tests/core/media/test_media_service.py
@@ -1,3 +1,4 @@
+import re
 import io
 from unittest import mock
 
@@ -9,9 +10,17 @@ from app.core.media.media_service import MediaService
 @pytest.mark.asyncio
 @mock.patch("boto3.resource")
 async def test_upload_image(mock_boto_resource):
-    media_service = MediaService("some-region", "some-bucket")
-
     img_content = io.BytesIO(b"hello world: \x00\x01")
-    result = await media_service.upload_image("img.png", img_content)
+    s3_url = "https://some-bucket.s3.some-region.amazonaws.com"
 
-    assert result == "https://some-bucket.s3.some-region.amazonaws.com/img.png"
+    # with random suffix
+    media_service = MediaService("some-region", "some-bucket", True)
+    result = await media_service.upload_image("img.png", img_content)
+    assert re.match(
+        r"(https://some-bucket\.s3\.some-region\.amazonaws\.com/img-.{22}.png)", result
+    )
+
+    # without suffix
+    media_service = MediaService("some-region", "some-bucket")
+    result = await media_service.upload_image("img.png", img_content)
+    assert result == f"{s3_url}/img.png"
