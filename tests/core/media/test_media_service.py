0d5a85967e8b8fa580d96754fa61bec1d6cedf5a
zwessels <zak@wcisolutions.com>
2022-03-21 11:50:18 +0200

    #27 fix unit test

--- tests/core/media/test_media_service.py
+++ tests/core/media/test_media_service.py
@@ -1,4 +1,3 @@
-import re
 import io
 from unittest import mock
 
@@ -16,14 +15,6 @@ s3_url = f"https://{bucket}.s3.{region}.amazonaws.com"
 async def test_upload_image(mock_boto_resource):
     img_content = io.BytesIO(b"hello world: \x00\x01")
 
-    # with random suffix
-    media_service = MediaService(region, bucket, True)
-    result = await media_service.upload_image("img.png", img_content)
-    assert re.match(
-        r"(https://some-bucket\.s3\.some-region\.amazonaws\.com/img-.{22}.png)", result
-    )
-
-    # without suffix
     media_service = MediaService(region, bucket)
     result = await media_service.upload_image("img.png", img_content)
     assert result == f"{s3_url}/img.png"
