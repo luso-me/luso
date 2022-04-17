886243e038d1e170e7e07389b8119eec0e8eaeb6
zwessels <zak@wcisolutions.com>
2022-04-17 22:21:58 +0200

    fix: add unit test for upload skill icon

--- tests/core/skill/test_skill_service.py
+++ tests/core/skill/test_skill_service.py
@@ -12,21 +12,17 @@ skill_service = SkillService()
 @pytest.mark.asyncio
 @mock.patch("app.core.media.media_service.MediaService.upload_image")
 @mock.patch("app.repositories.skill.SkillRepository.update")
-async def test_update_skill_icon(mock_media_service, mock_skill_repo):
-    # mock_media_service.return_value = "https://s3.com/some-icon-link.svg"
-    mock_skill_repo.return_value = _create_skill()
+@mock.patch("app.core.skill.skill_service.SkillService.get_skill")
+async def test_update_skill_icon(
+    mock_media_service, mock_skill_repo, mock_skill_service
+):
+    skill = _create_skill()
+    mock_skill_repo.return_value = skill
 
-    # step 1: prove that the current code fails
-
-    # get the resources on the return value
-
-    img_content = io.BytesIO(b"hello world: \x00\x01")
-    x = await skill_service.update_skill_icon(
-        "123", "some-skill", "some_icon", img_content
+    result_skill = await skill_service.update_skill_icon(
+        "123", "some-skill", "some_icon", io.BytesIO(b"hello world: \x00\x01")
     )
-    assert len(x.resources) == 2
-
-    # step 2: change the code so that it passes
+    assert len(result_skill.resources) == 2
 
 
 def _create_skill():
