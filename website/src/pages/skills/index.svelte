b08f12341ee0d6fe12a02bfd62ef4e9370eb6c0f
zwessels <zak@wcisolutions.com>
2022-04-25 21:23:39 +0200

    feat: add guidance for adding/changing skills & resources

--- website/src/pages/skills/index.svelte
+++ website/src/pages/skills/index.svelte
@@ -40,7 +40,18 @@
           </Button>
         </Cell>
       {:else}
-        Don
+        <Cell span="{12}">
+          <Button href="https://github.com/luso-me/luso-technology/issues/new?assignees=&labels=&template=add-skill.yml"
+                  target="_blank" variant="raised">
+            <Icon class="material-icons">help</Icon>
+            <Label>Don't see your Skill listed?</Label>
+          </Button>
+          <Button href="https://github.com/luso-me/luso-technology/issues/new?assignees=&labels=&template=change-skill.yml"
+                  target="_blank" variant="raised">
+            <Icon class="material-icons">help</Icon>
+            <Label>Found a mistake?</Label>
+          </Button>
+        </Cell>
       {/if}
       <Cell span="{12}">
         <DataTable class="luso-width-100-percent">
